c1583d04f49202d380a2db170c105b6fee855452
Adi Mazuz <adi.mazuz@intel.com>
2021-07-14 12:53:59 +0300

    dynamcily adding projects

---
+++ script.js
@@ -0,0 +1,23 @@
+const projectsList = document.getElementById("projects-list");
+
+const projects = ["progress-steps", "expanding-cards", "rotating-navigation"];
+
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitStr.join(" ");
+}
+
+projects.forEach((folderName) => {
+  const projectName = folderName.replace("-", " ");
+  const projectTitle = titleCase(projectName);
+  const element = `<li> <span class="fa-li"><i class="fas fa-angle-double-right"></i></i></span> <a href="./${folderName}/">${projectTitle}</a> </li>`;
+
+  projectsList.innerHTML += element;
+});
